d516238acf6596ff5abde2585e9702f154cea73f
Loong Yong En <yongen.loong@aelf.io>
2023-09-16 18:14:48 +0800

    fix: tests

--- libs/ledgerjs/packages/hw-app-elf/tests/Elf.unit.test.ts
+++ libs/ledgerjs/packages/hw-app-elf/tests/Elf.unit.test.ts
@@ -29,8 +29,8 @@ describe("AElf app binding", () => {
   test("getAddress", async () => {
     const transport = await openTransportReplayer(
       RecordStore.fromString(`
-      => e002000015058000002c80000650800000008000000000000000
-      <= 044ba9033c0b7fc04e8597536aa2367aaef14ae164f9ef39789e1a9418b52615499a146c0221ce7248e4a885f4d966e1e4cb29bcf0aa71f1557bfde07b01e785189000
+      => e002000015058000002c80000650800000000000000000000000
+      <= 04a1a2d994e07e7f34ed6d3b6850bb7c534ee481aa52d6a076d52667e3efefc2d840ab9e4a9be21d42f2802deb7ceee31a13a62646deba6745429ad8654d30d2c19000
       `),
     );
 
@@ -38,17 +38,17 @@ describe("AElf app binding", () => {
     const result = await elf.getAddress(DERIVATION_PATH);
 
     expect(result).toEqual({
-      address: "qC9LpZnZ1CPzkEQPLjosdtb5DJaSjGg5eTrq738Dbi49HYDev",
+      address: "d41uXjj7AmvDjkCU9bvfWpZdYEjpVyxmTBSc6fsdkB2L4dGos",
       publicKey:
-        "044ba9033c0b7fc04e8597536aa2367aaef14ae164f9ef39789e1a9418b52615499a146c0221ce7248e4a885f4d966e1e4cb29bcf0aa71f1557bfde07b01e78518",
+        "04a1a2d994e07e7f34ed6d3b6850bb7c534ee481aa52d6a076d52667e3efefc2d840ab9e4a9be21d42f2802deb7ceee31a13a62646deba6745429ad8654d30d2c1",
     });
   });
 
   test("getAddress with display", async () => {
     const transport = await openTransportReplayer(
       RecordStore.fromString(`
-      => e002010015058000002c80000650800000008000000000000000
-      <= 044ba9033c0b7fc04e8597536aa2367aaef14ae164f9ef39789e1a9418b52615499a146c0221ce7248e4a885f4d966e1e4cb29bcf0aa71f1557bfde07b01e785189000
+      => e002010015058000002c80000650800000000000000000000000
+      <= 04a1a2d994e07e7f34ed6d3b6850bb7c534ee481aa52d6a076d52667e3efefc2d840ab9e4a9be21d42f2802deb7ceee31a13a62646deba6745429ad8654d30d2c19000
       `),
     );
 
@@ -56,17 +56,17 @@ describe("AElf app binding", () => {
     const result = await elf.getAddress(DERIVATION_PATH, true);
 
     expect(result).toEqual({
-      address: "qC9LpZnZ1CPzkEQPLjosdtb5DJaSjGg5eTrq738Dbi49HYDev",
+      address: "d41uXjj7AmvDjkCU9bvfWpZdYEjpVyxmTBSc6fsdkB2L4dGos",
       publicKey:
-        "044ba9033c0b7fc04e8597536aa2367aaef14ae164f9ef39789e1a9418b52615499a146c0221ce7248e4a885f4d966e1e4cb29bcf0aa71f1557bfde07b01e78518",
+        "04a1a2d994e07e7f34ed6d3b6850bb7c534ee481aa52d6a076d52667e3efefc2d840ab9e4a9be21d42f2802deb7ceee31a13a62646deba6745429ad8654d30d2c1",
     });
   });
 
   test("getAddress with chainCode", async () => {
     const transport = await openTransportReplayer(
       RecordStore.fromString(`
-      => e002000015058000002c80000650800000008000000000000000
-      <= 044ba9033c0b7fc04e8597536aa2367aaef14ae164f9ef39789e1a9418b52615499a146c0221ce7248e4a885f4d966e1e4cb29bcf0aa71f1557bfde07b01e785189000
+      => e002000015058000002c80000650800000000000000000000000
+      <= 04a1a2d994e07e7f34ed6d3b6850bb7c534ee481aa52d6a076d52667e3efefc2d840ab9e4a9be21d42f2802deb7ceee31a13a62646deba6745429ad8654d30d2c19000
       `),
     );
 
@@ -74,9 +74,9 @@ describe("AElf app binding", () => {
     const result = await elf.getAddress(DERIVATION_PATH, false, true);
 
     expect(result).toEqual({
-      address: "qC9LpZnZ1CPzkEQPLjosdtb5DJaSjGg5eTrq738Dbi49HYDev",
+      address: "d41uXjj7AmvDjkCU9bvfWpZdYEjpVyxmTBSc6fsdkB2L4dGos",
       publicKey:
-        "044ba9033c0b7fc04e8597536aa2367aaef14ae164f9ef39789e1a9418b52615499a146c0221ce7248e4a885f4d966e1e4cb29bcf0aa71f1557bfde07b01e78518",
+        "04a1a2d994e07e7f34ed6d3b6850bb7c534ee481aa52d6a076d52667e3efefc2d840ab9e4a9be21d42f2802deb7ceee31a13a62646deba6745429ad8654d30d2c1",
       chainCode: "9000",
     });
   });
@@ -84,20 +84,20 @@ describe("AElf app binding", () => {
   test("signTransaction", async () => {
     const transport = await openTransportReplayer(
       RecordStore.fromString(`
-      => e0030100b101058000002c800006508000000080000000000000000a220a20d736f33c7c2a35a04603fd3e94d5395c29edd9ac159bed03e366c8fb700b7d1812220a202791e992a57f28e75a11f13af2c0aec8b0eb35d2f048d42eba8901c92e0378dc18b2fbe054220475e754232a085472616e73666572323c0a220a204ff4e63ad4aa7ec92e65ba2d37b2c56b3f82390bfc25e66cebab6821f3b05c0b1203454c461880d4dbd20f220b612074657374206d656d6f
-      <= 5e4bff058f083935f665bcb6419f918aef5c87fa0aedbd3f51343173d1ca9aad7b2d81e5403ee0a693f6d0900cca08cda8a363b4a88ad537ada0047f32e86231009000
+      => e0030100b101058000002c800006508000000000000000000000000a220a20d736f33c7c2a35a04603fd3e94d5395c29edd9ac159bed03e366c8fb700b7d1812220a202791e992a57f28e75a11f13af2c0aec8b0eb35d2f048d42eba8901c92e0378dc18c7ab95552204a13980492a085472616e73666572323c0a220a204ff4e63ad4aa7ec92e65ba2d37b2c56b3f82390bfc25e66cebab6821f3b05c0b1203454c461880d4dbd20f220b612074657374206d656d6f
+      <= fdebf82f946f7e5acbaf3237f9725ed89f462e4cb786441bf458db5a3cb5cbf85380eb3321c3dbc9943f022ce6d8978f46baadbfd5eea8bf097bed5ae6312ec5009000
       `),
     );
 
     const elf = new Elf(transport);
     const result = await elf.signTransaction(
       DERIVATION_PATH,
-      "0a220a20d736f33c7c2a35a04603fd3e94d5395c29edd9ac159bed03e366c8fb700b7d1812220a202791e992a57f28e75a11f13af2c0aec8b0eb35d2f048d42eba8901c92e0378dc18b2fbe054220475e754232a085472616e73666572323c0a220a204ff4e63ad4aa7ec92e65ba2d37b2c56b3f82390bfc25e66cebab6821f3b05c0b1203454c461880d4dbd20f220b612074657374206d656d6f",
+      "0a220a20d736f33c7c2a35a04603fd3e94d5395c29edd9ac159bed03e366c8fb700b7d1812220a202791e992a57f28e75a11f13af2c0aec8b0eb35d2f048d42eba8901c92e0378dc18c7ab95552204a13980492a085472616e73666572323c0a220a204ff4e63ad4aa7ec92e65ba2d37b2c56b3f82390bfc25e66cebab6821f3b05c0b1203454c461880d4dbd20f220b612074657374206d656d6f",
     );
 
     expect(result).toEqual({
       signature:
-        "5e4bff058f083935f665bcb6419f918aef5c87fa0aedbd3f51343173d1ca9aad7b2d81e5403ee0a693f6d0900cca08cda8a363b4a88ad537ada0047f32e8623100",
+        "fdebf82f946f7e5acbaf3237f9725ed89f462e4cb786441bf458db5a3cb5cbf85380eb3321c3dbc9943f022ce6d8978f46baadbfd5eea8bf097bed5ae6312ec500",
       chainCode: "9000",
     });
   });
