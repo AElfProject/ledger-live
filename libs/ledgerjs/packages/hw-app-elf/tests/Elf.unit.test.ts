06709db6570518dec704164b26179cc2b570afae
Loong Yong En <yongen.loong@aelf.io>
2023-09-16 17:05:02 +0800

    fix: derivation path

--- libs/ledgerjs/packages/hw-app-elf/tests/Elf.unit.test.ts
+++ libs/ledgerjs/packages/hw-app-elf/tests/Elf.unit.test.ts
@@ -4,6 +4,8 @@ import Elf from "../src/Elf";
 jest.mock("axios");
 
 describe("AElf app binding", () => {
+  const DERIVATION_PATH = "m/44'/1616'/0'/0'/0";
+
   beforeEach(() => {
     jest.resetAllMocks();
   });
@@ -33,7 +35,7 @@ describe("AElf app binding", () => {
     );
 
     const elf = new Elf(transport);
-    const result = await elf.getAddress("44'/1616'/0'/0'/0");
+    const result = await elf.getAddress(DERIVATION_PATH);
 
     expect(result).toEqual({
       address: "qC9LpZnZ1CPzkEQPLjosdtb5DJaSjGg5eTrq738Dbi49HYDev",
@@ -51,7 +53,7 @@ describe("AElf app binding", () => {
     );
 
     const elf = new Elf(transport);
-    const result = await elf.getAddress("44'/1616'/0'/0'/0", true);
+    const result = await elf.getAddress(DERIVATION_PATH, true);
 
     expect(result).toEqual({
       address: "qC9LpZnZ1CPzkEQPLjosdtb5DJaSjGg5eTrq738Dbi49HYDev",
@@ -69,7 +71,7 @@ describe("AElf app binding", () => {
     );
 
     const elf = new Elf(transport);
-    const result = await elf.getAddress("44'/1616'/0'/0'/0", false, true);
+    const result = await elf.getAddress(DERIVATION_PATH, false, true);
 
     expect(result).toEqual({
       address: "qC9LpZnZ1CPzkEQPLjosdtb5DJaSjGg5eTrq738Dbi49HYDev",
@@ -89,7 +91,7 @@ describe("AElf app binding", () => {
 
     const elf = new Elf(transport);
     const result = await elf.signTransaction(
-      "44'/1616'/0'/0'/0",
+      DERIVATION_PATH,
       "0a220a20d736f33c7c2a35a04603fd3e94d5395c29edd9ac159bed03e366c8fb700b7d1812220a202791e992a57f28e75a11f13af2c0aec8b0eb35d2f048d42eba8901c92e0378dc18b2fbe054220475e754232a085472616e73666572323c0a220a204ff4e63ad4aa7ec92e65ba2d37b2c56b3f82390bfc25e66cebab6821f3b05c0b1203454c461880d4dbd20f220b612074657374206d656d6f",
     );
 
